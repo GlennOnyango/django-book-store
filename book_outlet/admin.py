63969c6d3172db1423ccb4b95f6203ff97a20a7a
GlennOnyango <glennonyango@yahoo.com>
2024-03-20 15:01:49 +0300

    first commit

---
+++ book_outlet/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Book
+# Register your models here.
+
+
+class BookAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug":("title",)}
+
+    list_display = ("title","author")
+
+    list_filter = ("rating",)
+
+admin.site.register(Book, BookAdmin)
